06cf0c29422c578de9dc3a9d113fcfcb92b1484b
EduardoRibeiroXavier <81425206+EduardoRibeiroXavier@users.noreply.github.com>
2023-11-17 11:19:04 -0300

    main

---
+++ Documentos/Projetos/React/iniciando-projeto/src/About-me.js
@@ -0,0 +1,15 @@
+import './About-me.css';
+
+function AboutMe() {
+      const  aboutMestyles = {
+      color: 'blue',  
+    };
+  return (
+    <>
+    <h1 style={aboutMestyles}>Meu nome é Eduardo Ribeiro.</h1>
+    <p>Estou aprendendo a programar usando o React!</p>
+    </>
+  ); 
+}
+
+export default AboutMe;
---
+++ Documentos/Projetos/React/iniciando-projeto/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import AboutMe from './About-me';
+
+function App() {
+  return (
+    <div class ="Container">
+      <h1>Hello, World</h1>
+      <p>Aqui se inicia um projeto em React!</p>
+      <AboutMe/> 
+    </div>
+  );
+}
+
+export default App;
